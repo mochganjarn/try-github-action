66ffa847e135bd640f4f07187a32121902a6d3b8
mochganjarn <mganjar.mg@gmail.com>
2021-09-26 12:18:48 +0700

    update

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/gofiber/fiber/v2 v2.19.0
+	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.7.0
 )
 
--- router/hello_world.go
+++ router/hello_world.go
@@ -1,13 +1,23 @@
 package router
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/mochganjarn/try-github-action/handler"
 )
 
 func Router(app *fiber.App) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
 	hello_route(app)
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
 
 func hello_route(hello *fiber.App) {
